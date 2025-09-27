898306896704e59479628d4a0229df1c86a8b02c
tylerzajac <34617047+tylerzajac@users.noreply.github.com>
2025-09-27 18:36:53 -0400

    Working final product. Play 5 rounds of Rock, Paper, Scissors.

--- script.js
+++ script.js
@@ -12,16 +12,63 @@ function getComputerChoice(number){
     }
 }
 
-console.log("Computer's choice: " + getComputerChoice(randomNumber));
-
 function getHumanChoice(){
-    const choice = prompt("Enter Rock, Paper, or Scissors");
+    let choice = prompt("Enter Rock, Paper, or Scissors");
 
-    return choice;
+    // standardize human input
+    const normHumanChoice = choice.toLowerCase();
+
+    return normHumanChoice;
 }
 
-console.log("Human's choice: " + getHumanChoice());
+
+
+let humanSelection;
+let computerSelection;
 
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice(randomNumber);
+    console.log("Human's choice: " + humanChoice);
+    console.log("Computer's choice: " + computerChoice);
+
+    if (humanChoice === 'rock' && computerChoice === 'Paper'){
+        console.log("You lose! Paper covers Rock.");
+        computerScore++;
+    } else if (humanChoice === 'rock' && computerChoice === "Rock") {
+        console.log("Round tie! No winners and no losers.");
+    } else if (humanChoice === 'rock' && computerChoice === 'Scissors') {
+        console.log("You win! Rock crushes scissors!");
+        humanScore++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'Paper') {
+        console.log("You win! Scissors cut paper.");
+        humanScore++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'Rock') {
+        console.log("You lose! Rock crushes scissors");
+        computerScore++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'Scissors') {
+        console.log("Round tie! No winners and no losers.");
+    } else if (humanChoice === 'paper' && computerChoice === 'Paper') {
+        console.log("Round tie! No winners and no losers.");
+    } else if (humanChoice === 'paper' && computerChoice === 'Rock') {
+        console.log("You win! Paper covers rock!");
+        humanScore++;
+    } else if (humanChoice === 'paper' && computerChoice === 'Scissors') {
+        console.log("You lose! Scissors cut paper.");
+        computerScore++;
+    }
+
+    console.log("Computer score: " + computerScore);
+    console.log("Human score: " + humanScore);
+}
+
+function playGame(number) {
+    for (let i = 0; i < number; i++) {
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+playGame(5);
